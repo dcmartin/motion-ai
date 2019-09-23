1a0e302462e9135b8be0783450a175a6c3263054
dcmartin <github@dcmartin.com>
2019-09-23 14:27:17 -0700

    dev
    Former-commit-id: 3253b5eb9a5d5ee0859a1e6e27eb4d4aa1562db6
    Former-commit-id: 1d25d8016699c3c34fe978ac63985a2b7957b903

--- redo.sh
+++ redo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker stop homeassistant
-sudo rm -fr /usr/share/hassio/homeassistant/.storage
 sudo rm -fr /usr/share/hassio/homeassistant/home-assistant*
 docker restart homeassistant
