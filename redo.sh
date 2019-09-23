cb5c45dceb0e9fd92889a8d4224b28a1ccc271c8
dcmartin <github@dcmartin.com>
2019-09-23 12:01:14 -0700

    dev

--- redo.sh
+++ redo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker stop homeassistant
+sudo rm /usr/share/hassio/homeassistant/.storage/core.restore_state
+sudo rm /usr/share/hassio/homeassistant/.storage/lovelace
 sudo rm -fr /usr/share/hassio/homeassistant/home-assistant*
 docker restart homeassistant
