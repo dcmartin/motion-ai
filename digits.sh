968e680602f147b9377a3b5802620096184096a9
dcmartin <github@dcmartin.com>
2019-09-25 09:26:37 -0700

    lgtm

--- digits.sh
+++ digits.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+ # curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey |   sudo apt-key add -
+ # apt update -qq -y
+ # apt upgrade -qq -y
+ # sudo apt install -y nvidia-driver-390
+ # sudo apt install -y nvidia-cuda-toolkit
+
 docker run \
   --name digits \
   -d \
