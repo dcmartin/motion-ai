c7cb46a606e98b38d98f346bcd763a55edd4ebc3
dcmartin <github@dcmartin.com>
2020-05-05 12:25:54 -0700

    lgtm
    Former-commit-id: 8f05822b8fd1b2a92165fde250cb950e534be75f
    Former-commit-id: 6f027355051f2548abd46e2ee26364d060568b1e
    Former-commit-id: 5d1757b3183061afbbdae857f85c895ae1df4c62

--- sh/ha4mac.sh
+++ sh/ha4mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f homeassistant
-docker run --init -d --name="homeassistant" -e "TZ=America/Los_Angeles" -v ~/GIT/motion-ai/homeassistant:/config -p 8123:8123 homeassistant/home-assistant:stable
+docker run --restart=unless-stopped --init -d --name="homeassistant" -e "TZ=America/Los_Angeles" -v ~/GIT/motion-ai/homeassistant:/config -p 8123:8123 homeassistant/home-assistant:stable
