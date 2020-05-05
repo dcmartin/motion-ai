12f4f2684e2f14963b445e328326d0ea87bc9570
dcmartin <github@dcmartin.com>
2020-05-05 08:22:24 -0700

    lgtm

--- sh/devices.sh
+++ sh/devices.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-MQTT_HOST=${MQTT_HOST:-mqtt}
-MQTT_USERNAME=${MQTT_USERNAME:-username}
-MQTT_PASSWORD=${MQTT_PASSWORD:-password}
+# MQTT
+if [ -z "${MQTT_HOST:-}" ] && [ -s MQTT_HOST ]; then MQTT_HOST=$(cat MQTT_HOST); fi; MQTT_HOST=${MQTT_HOST:-$(hostname -I | awk '{ print $1 }')}
+if [ -z "${MQTT_USERNAME:-}" ] && [ -s MQTT_USERNAME ]; then MQTT_USERNAME=$(cat MQTT_USERNAME); fi; MQTT_USERNAME=${MQTT_USERNAME:-username}
+if [ -z "${MQTT_PASSWORD:-}" ] && [ -s MQTT_PASSWORD ]; then MQTT_PASSWORD=$(cat MQTT_PASSWORD); fi; MQTT_PASSWORD=${MQTT_PASSWORD:-password}
+if [ -z "${MQTT_PORT:-}" ] && [ -s MQTT_PORT ]; then MQTT_PORT=$(cat MQTT_PORT); fi; MQTT_PORT=${MQTT_PORT:-1883}
+MQTT='{"host":"'${MQTT_HOST}'","port":'${MQTT_PORT}',"username":"'${MQTT_USERNAME}'","password":"'${MQTT_PASSWORD}'"}'
+
+## MOTION
+if [ -z "${MOTION_GROUP:-}" ] && [ -s MOTION_GROUP ]; then MOTION_GROUP=$(cat MOTION_GROUP); fi; MOTION_GROUP=${MOTION_GROUP:-motion}
+if [ -z "${MOTION_CLIENT:-}" ] && [ -s MOTION_CLIENT ]; then MOTION_CLIENT=$(cat MOTION_CLIENT); fi; MOTION_CLIENT=${MOTION_CLIENT:-+}
+if [ -z "${MOTION_CAMERA:-}" ] && [ -s MOTION_CAMERA ]; then MOTION_CAMERA=$(cat MOTION_CAMERA); fi; MOTION_CAMERA=${MOTION_CAMERA:-+}
+MOTION='{"group":"'${MOTION_GROUP}'","client":"'${MOTION_CLIENT}'","camera":"'${MOTION_CAMERA}'"}'
 
 TOPIC="+/+/start"
 DEVICES='[]'
@@ -11,7 +20,7 @@ BEGIN=$(date +%s)
 
 echo "--- INFO $0 $$ -- listening for topic ${TOPIC}" &> /dev/stderr
 
-mosquitto_sub -h ${MQTT_HOST} -u username -P password -t ${TOPIC} | while read -r; do
+mosquitto_sub -h ${MQTT_HOST} -u ${MQTT_USERNAME} -P ${MQTT_PASSWORD} -t ${TOPIC} | while read -r; do
 
     NOW=$(date +%s)
 
