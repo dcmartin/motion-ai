22a13cb00f1495f7f9aa1ec8d1806e2eab3d45fd
dcmartin <github@dcmartin.com>
2020-05-05 13:41:01 -0700

    lgtm

--- sh/restart.sh
+++ sh/restart.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-for i in $(cat NODES); do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && git pull && make restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
+if [ -z "${*:-}" ]; then NODES=$(cat NODES); else NODES=${*}; fi
+
+for i in ${NODES}; do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && git pull && make restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
