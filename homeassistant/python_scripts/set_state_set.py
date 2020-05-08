773cb4c7ee07daa86e9fa0e7b710775d0311ae8d
dcmartin <github@dcmartin.com>
2020-05-08 12:34:24 -0700

    lgtm
    Former-commit-id: 1b61a0c3f585500fa1314b3c5d3c7d2bccb51d3c
    Former-commit-id: 9d91cc4d567ba12b1bbf8242ef7b206cc7ba1797
    Former-commit-id: d39e9e4dbc871124fa787c0d01087f8aa3b4df1c

---
+++ homeassistant/python_scripts/set_state_set.py
@@ -0,0 +1,49 @@
+###
+## Original: https://github.com/rodpayne/home-assistant/blob/master/python_scripts/set_state.py
+###
+
+inputEntity = data.get('entity_id')
+if inputEntity is None:
+    logger.error("set_state: missing required argument: entity_id")
+else:
+    inputStateObject = hass.states.get(inputEntity)
+    if inputStateObject is None and not data.get('allow_create'):
+        logger.warning("set_state: unknown entity_id: {0}; allow_create is not set".format(inputEntity))
+    else:
+        logger.debug("set_state: entity_id: {0}".format(inputEntity))
+        if not inputStateObject is None:
+          inputState = inputStateObject.state
+          inputAttributesObject = inputStateObject.attributes.copy()
+        else:
+          inputAttributesObject = {}
+    
+        # process entity attribute value updates 
+        for item in data:
+
+            # excluding 'entity_id', 'allow_create'
+            if item == 'entity_id':
+              logger.debug("set_state: skipping {0}".format(item))
+              continue
+            elif item == 'allow_create':
+              logger.debug("set_state: skipping {0}".format(item))
+              continue
+
+            if item == 'state':
+              # state can only be string
+              inputState = data.get(item)
+              logger.debug("set_state: updating entity state; item: {0}; value: {1}".format(item,inputState))
+            else:
+              attr_value = data.get(item)
+
+              obj = attr_value.split(',') 
+              if len(obj) > 0:
+                obj=list(set(obj))
+                logger.debug("set_state: updating entity attribute array; item: {0}; value: {1}".format(item,attr_value))
+                inputAttributesObject[item] = obj
+              else:
+                logger.debug("set_state: updating entity attribute value; item: {0}; value: {1}".format(item,attr_value))
+                inputAttributesObject[item] = attr_value
+
+        # update
+        logger.debug("set_state: updating; entity: ".format(inputEntity))
+        hass.states.set(inputEntity, inputState, inputAttributesObject)
