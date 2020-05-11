4899504ccf4908f00f7748b35d08b32dd3b61437
dcmartin <github@dcmartin.com>
2020-05-11 12:12:58 -0700

    lgtm

--- sh/ha4mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker rm -f homeassistant
-docker run --restart=unless-stopped --init -d --name="homeassistant" -e "TZ=America/Los_Angeles" -v ~/GIT/motion-ai/homeassistant:/config -p 8123:8123 homeassistant/home-assistant:stable
