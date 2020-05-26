36fd4d0b40977f871b7a330b5b4cca70337f96e6
dcmartin <github@dcmartin.com>
2020-05-26 14:44:31 -0700

    lgtm

--- sh/restart.sh
+++ sh/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ -z "${*:-}" ]; then NODES=$(cat NODES); else NODES=${*}; fi
+if [ -z "${*:-}" ]; then NODES=$(egrep -v '^#' NODES); else NODES=${*}; fi
 
 for i in ${NODES}; do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && git pull && make restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
