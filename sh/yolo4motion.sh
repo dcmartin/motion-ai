1a1c411038cefd26c4a16dd586dc9a23649dd302
dcmartin <github@dcmartin.com>
2020-06-18 00:03:20 +0100

    lgtm
    Former-commit-id: 17c051885b570d8e224cb4da2a74dd5c70e5a297
    Former-commit-id: b8d5f94bf6e6109565c3352511538660d8d4c493
    Former-commit-id: 0b97e5bda47f66d38549afe847c42e1bfc36dc41

--- sh/yolo4motion.sh
+++ sh/yolo4motion.sh
@@ -70,7 +70,7 @@ LOG='{"debug":'${DEBUG}',"level":"'"${LOG_LEVEL}"'","logto":"'"${LOGTO}"'"}'
 
 ## SERVICE
 if [ -z "${CONTAINER_TAG:-}" ] && [ -s CONTAINER_TAG ]; then CONTAINER_TAG=$(cat CONTAINER_TAG); fi; CONTAINER_TAG=${CONTAINER_TAG:-0.1.2}
-SERVICE='{"label":"yolo4motion","id":"'${CONTAINER_ID}'","tag":"'${CONTAINER_TAG}'","arch":"'${SERVICE_ARCH:-${BUILD_ARCH}}'","ports":{"service":'${SERVICE_PORT:-80}',"host":'${HOST_PORT:-4662}'},"mount":[{"source":"'${PWD}'/yolov2-tiny.weights","target":"/openyolo/darknet/yolov2-tiny.weights"},{"source":"'${PWD}'/yolov3-tiny.weights","target":"/openyolo/darknet/yolov3-tiny.weights"},{"source":"'${PWD}'/yolov2.weights","target":"/openyolo/darknet/yolov2.weights"},{"source":"'${PWD}'/yolov3.weights","target":"/openyolo/darknet/yolov3.weights"}]}'
+SERVICE='{"label":"yolo4motion","id":"'${CONTAINER_ID}'","tag":"'${CONTAINER_TAG}'","arch":"'${SERVICE_ARCH:-${BUILD_ARCH}}'","ports":{"service":'${SERVICE_PORT:-80}',"host":'${HOST_PORT:-4662}'},"mount":[{"source":"'${PWD}'/yolov2-tiny-voc.weights","target":"/openyolo/darknet/yolov2-tiny-voc.weights"},{"source":"'${PWD}'/yolov3-tiny.weights","target":"/openyolo/darknet/yolov3-tiny.weights"},{"source":"'${PWD}'/yolov2.weights","target":"/openyolo/darknet/yolov2.weights"},{"source":"'${PWD}'/yolov3.weights","target":"/openyolo/darknet/yolov3.weights"}]}'
 
 # extra mounts
 if [ $(echo "${SERVICE}" | jq '.mount!=null') = true ]; then
