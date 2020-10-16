c6413e880e5fdab5d611277acaf00509c3aca128
dcmartin <github@dcmartin.com>
2020-10-15 17:06:51 -0700

    status is on-line or off-line

--- sh/refresh.sh
+++ sh/refresh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -z "${*:-}" ]; then NODES=$(egrep -v '^#' NODES); else NODES=${*}; fi
 
-for i in ${NODES}; do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && git pull && make realclean restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
+for i in ${NODES}; do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && git checkout master && git pull && make realclean restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
