bef7f6fef7622369a10d7abb41b6db22c52c884f
dcmartin <dcmartin@base-debian-10.localdomain>
2020-10-17 11:41:56 -0700

    updated to latest

--- sh/hassio-install.sh
+++ sh/hassio-install.sh
@@ -7,6 +7,16 @@ function info { echo -e "\e[32m[info] $*\e[39m"; }
 function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 
+warn ""
+warn "If you want more control over your own system, run"
+warn "Home Assistant as a VM or run Home Assistant Core"
+warn "via a Docker container."
+warn ""
+warn "If you want to abort, hit ctrl+c within 10 seconds..."
+warn ""
+
+sleep 10
+
 ARCH=$(uname -m)
 
 IP_ADDRESS=$(hostname -I | awk '{ print $1 }')
@@ -87,8 +97,12 @@ fi
 
 # Create config for NetworkManager
 info "Creating NetworkManager configuration"
+curl -sL "${URL_NM_CONF}" > "${FILE_NM_CONF}"
+if [ ! -f "$FILE_NM_CONNECTION" ]; then
+    curl -sL "${URL_NM_CONNECTION}" > "${FILE_NM_CONNECTION}"
+fi
 
-warn "Changes is needed to the /etc/network/interfaces file"
+warn "Changes are needed to the /etc/network/interfaces file"
 info "If you have modified the network on the host manualy, those can now be overwritten"
 info "If you do not overwrite this now you need to manually adjust it later"
 info "Do you want to proceed with that? [N/y] "
