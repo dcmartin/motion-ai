63b90f29ea8f4cb3b0d9dc9b77b8afe72b143f62
dcmartin <github@dcmartin.com>
2020-11-01 16:07:08 -0800

    lgtm

--- sh/mkwebcams.sh
+++ sh/mkwebcams.sh
@@ -3,7 +3,7 @@
 config=$(curl -sSL http://localhost:7999/cgi-bin/config 2> /dev/null)
 
 if [ ! -z "${config:-}" ]; then
-  echo "${config}" | jq '.config.motion.username as $u|.config.motion.password as $p|.config.cameras|to_entries|map({name:.value.name,top:(10+5*.key),left:10,icon:"cctv",w3w:null,mjpeg_url:.value.mjpeg_url,username:$u,password:$p})'
+  echo "${config}" | jq '.config.motion.username as $u|.config.motion.password as $p|.config.cameras|to_entries|map({name:.value.name,type:.value.type,top:(10+5*.key),left:10,icon:"cctv",w3w:null,mjpeg_url:.value.mjpeg_url,username:$u,password:$p})'
 else
   echo "No configuration from localhost:7999/cgi-bin/config; is motion add-on running?" &> /dev/stderr
 fi
