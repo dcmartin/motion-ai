06e533543442414431af40c06063a7c7822c9fb0
dcmartin <github@dcmartin.com>
2020-11-04 19:50:07 -0800

    lgtm

--- sh/mkln.sh
+++ sh/mkln.sh
@@ -2,17 +2,22 @@
 
 d=($(find motion/ -name *.gif -print | while read; do echo "${REPLY##* }"; done | sort | uniq | sed "s/.gif//")) 
 
+echo "Found ${#d[@]} cameras" &> /dev/stderr
+
 for c in ${d[@]}; do 
-  echo -n "Processing ${c}: "
+  echo -n "Processing camera ${c}: "
   rm -fr "${c}"
   mkdir -p "${c}" 
   pushd "${c}" &> /dev/null
   for i in ../motion/*; do 
-    for j in $i/*; do 
-      rm -fr "${i##*/}"
-      mkdir -p "${i##*/}/${j##*/}"
-      pushd "${i##*/}/${j##*/}" &> /dev/null
-      find ../../"${j}" -name '*'"${c}"'.gif' -print | while read; do 
+    rm -fr "${i##*/}"
+    mkdir "${i##*/}"
+    pushd "${i##*/}" &> /dev/null
+    for j in ../$i/*; do 
+      rm -fr "${j##*/}"
+      mkdir -p "${j##*/}"
+      pushd "${j##*/}" &> /dev/null
+      find "../${j}" -name "*${c}.gif" -print | while read; do 
         t="${REPLY##*/}" 
 	rm -f "${t}"
 	ln -s "${REPLY}" "${t}"
@@ -20,6 +25,7 @@ for c in ${d[@]}; do
       done
       popd &> /dev/null
     done
+    popd &> /dev/null
   done
   popd &> /dev/null
   echo
