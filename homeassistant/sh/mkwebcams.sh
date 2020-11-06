b8777902f96e5a657704298452ddd2ad41bae658
dcmartin <github@dcmartin.com>
2020-11-05 17:22:15 -0800

    better

--- homeassistant/sh/mkwebcams.sh
+++ homeassistant/sh/mkwebcams.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-config=$(curl -sSL http://localhost:7999/cgi-bin/config 2> /dev/null)
 
-if [ ! -z "${config:-}" ]; then
-  echo "${config}" | jq '[.config.cameras[]|{name:.name,type:.type,top:.top,left:.left,icon:.icon,w3w:.w3w,mjpeg_url:.mjpeg_url,username:.username,password:.password}]'
-else
-  echo "No configuration from localhost:7999/cgi-bin/config; is motion add-on running?" &> /dev/stderr
-fi
+function get_config()
+{
+  local url=${1}
+  local json=${2}
+  local tmp=$(mktemp)
+  curl -sSL -m 2 "${url}" -o "${tmp}" 2> /dev/null
+  if [ ! -s "${tmp}" ]; then
+    echo "No configuration from ${url}; is motion add-on running?" &> /dev/stderr
+    return
+  fi
+  jq '.' "${tmp}" > "${json}" 2> /dev/null
+  rm -f "${tmp}"
+}
+
+###
+## MAIN
+###
+
+url='http://localhost:7999/cgi-bin/config'
+json='homeassistant/motion/config.json'
+
+# get the configuration from the motion addon
+get_config "${url}" "${json}"
+
+config=$(jq -c '.' "${json}" 2> /dev/null)
+
+echo "${config:-[]}" \
+  | jq '[.config?.cameras?[]|{name:.name,type:.type,top:.top,left:.left,icon:.icon,w3w:.w3w,mjpeg_url:.mjpeg_url,username:.username,password:.password}]' \
+  && exit 0 \
+  || exit 1
