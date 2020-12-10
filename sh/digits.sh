ed7146ff913d13c834a7b45986d4ec11c2335f62
dcmartin <github@dcmartin.com>
2020-12-10 13:52:04 -0800

    versions

--- sh/digits.sh
+++ sh/digits.sh
@@ -9,6 +9,12 @@
 DATA_DIR=${HOME}/.digits/data
 JOBS_DIR=${HOME}/.digits/jobs
 
+ORIGINAL='nvidia/digits'
+STABLE='nvcr.io/nvidia/digits:20.06-tensorflow-py3'
+LATEST='nvcr.io/nvidia/digits:20.11-tensorflow-py3'
+
+TAG="${LATEST}"
+
 mkdir -p ${DATA_DIR} ${JOBS_DIR}
 
 docker run \
@@ -19,4 +25,4 @@ docker run \
   -v digits-data:${DATA_DIR} \
   -v digits-jobs:${JOBS_DIR} \
   --runtime=nvidia \
-  nvidia/digits
+  ${TAG}
