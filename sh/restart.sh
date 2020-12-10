4d20dc7aec750a8017012027d88cb96e07425e7e
dcmartin <github@dcmartin.com>
2020-12-09 19:42:02 -0800

    lgtm

--- sh/restart.sh
+++ sh/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -z "${*:-}" ]; then NODES=$(egrep -v '^#' NODES); else NODES=${*}; fi
 
-for i in ${NODES}; do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && echo 192.168.1.50 > MQTT_HOST && git pull && make restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
+for i in ${NODES}; do ping -t 15 -c 1 $i &> /dev/null && ssh $i 'cd GIT/motion-ai && git pull && make restart' &> /dev/null && echo "$i success" || echo "$i fail"; done
