6c54edbdaff17ee7784beda1c3ff4e771549494b
dcmartin <github@dcmartin.com>
2021-03-20 11:11:34 -0700

    lgtm

--- homeassistant/motion/sh/mkdots.sh
+++ homeassistant/motion/sh/mkdots.sh
@@ -7,17 +7,18 @@ function png.make()
   local name=${2:-$(mktemp)}
   local size=${3:-256x256}
 
-  convert -size ${size} xc:#${color} ${name}.png && echo "${name}.png"
+  convert -size ${size} xc:\#${color} ${name}.png && echo "${name}.png"
 }
 
 # convert PNG into 2x2 transparent/non-transparent mask
 function png.dots()
 {
   local file=${1:-}
+  local size=${2:-256x256}
   local out=${file%*.png}_dots.png
 
   if [ ! -z "${file:-}" ]; then
-    magick ${file} -set option:dim "%wx%h" \( -size 2x2 xc:black -size 1x1 xc:white -gravity northwest -composite -write mpr:tile +delete -size "%[dim]" tile:mpr:tile \) -alpha off -compose copy_opacity -composite ${out} && echo "${out}"
+    convert ${file} \( -size 2x2 xc:black -size 1x1 xc:white -gravity northwest -composite -write mpr:tile +delete -size "${size}" tile:mpr:tile \) -alpha off -compose copy_opacity -composite ${out} && echo "${out}"
   fi
 }
 
