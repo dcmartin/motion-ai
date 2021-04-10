04088bc0ea7faccee1e4dcf8639f5abed57918a1
dcmartin <github@dcmartin.com>
2021-04-09 20:49:30 -0700

    lgtm

--- homeassistant/custom_components/favicon/config_flow.py
+++ homeassistant/custom_components/favicon/config_flow.py
@@ -6,6 +6,7 @@ from homeassistant.core import callback
 
 _LOGGER = logging.getLogger(__name__)
 
+
 @config_entries.HANDLERS.register("favicon")
 class ConfigFlow(config_entries.ConfigFlow):
     async def async_step_user(self, user_input=None):
@@ -33,6 +34,7 @@ class ConfigFlow(config_entries.ConfigFlow):
     def async_get_options_flow(config_entry):
         return EditFlow(config_entry)
 
+
 class EditFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
@@ -46,12 +48,20 @@ class EditFlow(config_entries.OptionsFlow):
             step_id='init',
             data_schema=vol.Schema(
                 {
-                    vol.Optional('title',
-                        description="test",
-                        default=self.config_entry.options.get("title", "")): str,
-                    vol.Optional('icon_path',
-                        default=self.config_entry.options.get("icon_path", "")): str,
+                    vol.Optional(
+                        'title',
+                        description={
+                            "suggested_value":
+                                self.config_entry.options.get("title", ""),
+                        }
+                    ): str,
+                    vol.Optional(
+                        'icon_path',
+                        description={
+                            "suggested_value":
+                                self.config_entry.options.get("icon_path", ""),
+                        }
+                    ): str,
                 }
             ),
         )
-
