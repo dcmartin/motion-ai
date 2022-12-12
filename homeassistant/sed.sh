352eecb8224baeee9c2640cf901814adaf88ab53
dcmartin <github@dcmartin.com>
2022-12-12 13:46:06 -0800

    lgtm

--- homeassistant/sed.sh
+++ homeassistant/sed.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" sensor/device/class/temperature.yaml > sensor/device/class/${i}.yaml; done 
+
 for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" automation/device/class/temperature.yaml > automation/device/class/${i}.yaml; done 
 
 for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" binary_sensor/device/class/temperature.yaml > binary_sensor/device/class/${i}.yaml; done 
