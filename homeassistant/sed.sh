82c22be94a8ed208fd3d70982da12be9d981caae
dcmartin <github@dcmartin.com>
2023-02-02 13:53:45 -0800

    added window & cleanup

--- homeassistant/sed.sh
+++ homeassistant/sed.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" sensor/device/class/temperature.yaml > sensor/device/class/${i}.yaml; done 
+c=($(jq -r '.[].class' device_class.json | egrep -v 'temperature'))
 
-for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" automation/device/class/temperature.yaml > automation/device/class/${i}.yaml; done 
-
-for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" binary_sensor/device/class/temperature.yaml > binary_sensor/device/class/${i}.yaml; done 
-
-for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" template/device/class/temperature.yaml > template/device/class/${i}.yaml; done 
-
-for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" group/device/class/temperature.yaml > group/device/class/${i}.yaml; done 
-
-for i in battery connectivity current door energy gas humidity illuminance light moisture motion occupancy opening plug power presence problem safety smoke tamper update vibration voltage ; do sed -e "s/temperature/${i}/g" binary_sensor/alarm/class/temperature.yaml > binary_sensor/alarm/class/${i}.yaml; done 
+for i in ${c[@]}; do 
+sed -e "s/temperature/${i}/g" sensor/device/class/temperature.yaml > sensor/device/class/${i}.yaml
+sed -e "s/temperature/${i}/g" automation/device/class/temperature.yaml > automation/device/class/${i}.yaml
+sed -e "s/temperature/${i}/g" binary_sensor/device/class/temperature.yaml > binary_sensor/device/class/${i}.yaml
+sed -e "s/temperature/${i}/g" template/device/class/temperature.yaml > template/device/class/${i}.yaml
+sed -e "s/temperature/${i}/g" group/device/class/temperature.yaml > group/device/class/${i}.yaml
+sed -e "s/temperature/${i}/g" binary_sensor/alarm/class/temperature.yaml > binary_sensor/alarm/class/${i}.yaml
+echo "DONE: ${i}"
+done 
